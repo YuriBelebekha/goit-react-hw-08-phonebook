3ed128a9b0578fb044287aec5ac3aaa055084f64
Yuri Belebekha <ybelebekha@gmail.com>
2023-04-28 23:47:27 +0300

    Changed some options Watch Component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -7,14 +7,18 @@ import Typography from '@mui/material/Typography';
 export const Loader = () => {
   return (
     <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', p: '20px' }}>
-      <Typography variant='h5' component='p' sx={{ fontWeight: 'bold', color: '#00f4ff' }}>
-        Refreshing User Page...
-      </Typography>
-
       <Watch
         width='60'        
         color='orange'
       />
+      
+      <Typography
+        variant='h5'
+        component='p'
+        sx={{ ml: '10px', fontWeight: 'bold', color: '#00f4ff' }}
+      >
+        Refreshing User Page...
+      </Typography>      
     </Box>
   );
 };
